e02427468d80aa886ecb90e5440693d8c9a57956
Jiyeong Seok <jiyeong.seok@lge.com>
2024-08-01 16:14:20 +0900

    test

--- src/fosslight_dependency/run_dependency_scanner.py
+++ src/fosslight_dependency/run_dependency_scanner.py
@@ -40,6 +40,7 @@ _exclude_dir = ['node_moduels', 'venv']
 def find_package_manager(input_dir, abs_path_to_exclude=[]):
     ret = True
     manifest_file_name = []
+    print('')
     for value in const.SUPPORT_PACKAE.values():
         if isinstance(value, list):
             manifest_file_name.extend(value)
